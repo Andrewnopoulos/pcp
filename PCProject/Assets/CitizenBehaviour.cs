8441cb4d3d81b258bca1be26e3729d038279eb1e
Andrew <andrew.giannopoulos@gmail.com>
2015-04-01 11:29:28 +1100

    Added simple spotlight and moving cube

--- PCProject/Assets/CitizenBehaviour.cs
+++ PCProject/Assets/CitizenBehaviour.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HelloWorld : MonoBehaviour {
+public class CitizenBehaviour : MonoBehaviour {
+
+    public float MovementSpeed;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.position += new Vector3(0, 0, MovementSpeed);
 	}
 }
---
+++ PCProject/Assets/SpotlightBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotlightBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Mathf.Sin(Time.time)/10, 0, Mathf.Cos(Time.time)/10);
+	}
+}
